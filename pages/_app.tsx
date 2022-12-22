96d438e76ddf6f939e5cfb54fc15a57e79f58250
scooo <scooomaker@foxmail.com>
2022-12-22 19:32:09 +0800

    fix bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="" href="/favicon.ico" />
+        <title>红楼梦</title>
+
+      </Head>
+      
+      
+      <Component {...pageProps} />
+    </>
+    )
 }
 
 export default MyApp
